137c7dce999a6e419feff44dcd1033a123f4cf75
Joao Victor <seuemail@gmail.com>
2026-04-27 20:39:37 -0300

    superuser password change

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-python manage.py create_admin --username=admin --email=admin@email.com --password=suasenha
+python manage.py create_admin --username=admin --email=admin@email.com --password=Admin596
 gunicorn app.wsgi:application --bind 0.0.0.0:8000
